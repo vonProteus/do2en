00790a53dad4456b84a84b2af5ab80b0f9a9309c
Maciej Krok <vonproteus+sarah.local@gmail.com>
2016-02-20 15:41:43 +0100

    works

--- script/do2en.py
+++ script/do2en.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python
-import sys, getopt
+import sys, getopt, locale
+from datetime import datetime
+from subprocess import call
+from operator import  itemgetter
+
 
 class do2en:
     def __init__(self):
         self.photoDir = ""
         self.entriesFile = ""
-        self.lines = []
+        self.notebook = ""
+        self.entries = []
+        self.locale='pl_PL'
+        self.dataEntryFormat = "%Y %B %d %H:%M"
+        self.titlePrefix = "Imported from Day One"
+        self.dataInTitleFormat = "%Y-%m-%d %H:%M"
+
+
         return
 
     def main(self, argv):
@@ -13,25 +24,32 @@ class do2en:
 
         print "Entries File is "+ self.entriesFile
         print "Photo Dir is "+ self.photoDir
+        print "Notebook is "+ self.notebook
 
         self.readEntries()
 
-        print self.lines[1]
+        self.sentToEvernotr()
 
     def parseParam(self, argv):
         try:
-            opts, args = getopt.getopt(argv,"he:p:",["entries-file=","photos-dir="])
+            opts, args = getopt.getopt(argv,"he:p:n:",["entries-file=","photos-dir=","evernote-notebook="])
         except getopt.GetoptError:
-            print sys.argv[0]+' -e <entries-file> -p <photos-dir>'
+            print sys.argv[0]+' -e <entries-file> -p <photos-dir> -n <evernote-notebook>'
             sys.exit(2)
         for opt, arg in opts:
             if opt == '-h':
-                print sys.argv[0]+' -e <entries-file> -p <photos-dir>'
+                print sys.argv[0]+' -e <entries-file> -p <photos-dir> -n <evernote-notebook>'
                 sys.exit()
             elif opt in ("-e", "--entries-file"):
                 self.entriesFile = arg
             elif opt in ("-p", "--photos-dir"):
                 self.photoDir = arg
+            elif opt in ("-n", "--evernote-notebook"):
+                self.notebook = arg
+
+        if (self.notebook == "" or self.entriesFile == ""):
+            print sys.argv[0]+' -e <entries-file> -p <photos-dir> -n <evernote-notebook>'
+            sys.exit(2)
 
     def readEntries(self):
         file = open( self.entriesFile )
@@ -40,10 +58,88 @@ class do2en:
         lines=file.read().split(splitBy)
         file.close()
 
-        for l in lines:
-            if(len(l) == 0):
+        for line in lines:
+            if(len(line) == 0):
                 continue
-            self.lines.append(splitBy + l)
+            self.entries.append(self.parseEntry(splitBy + line))
+
+    def parseEntry(self, entryString):
+        lines=entryString.split("\n");
+        ansDict = {"date":"",
+                   "tags":"",
+                   "location":"",
+                   "weather":"",
+                   "photo":"",
+                   "text":"",
+                   }
+        locale.setlocale(locale.LC_ALL, self.locale)
+
+        for line in lines:
+            if(len(line) == 0):
+                continue
+            if(line.startswith("\tDate:\t")):
+                line = line.replace("\tDate:\t","")
+                ansDict['date'] = datetime.strptime(line, self.dataEntryFormat)
+                continue
+            if(line.startswith("\tLocation:\t")):
+                line = line.replace("\tLocation:\t","")
+                ansDict['location'] = line
+                continue
+            if(line.startswith("\tWeather:\t")):
+                line = line.replace("\tWeather:\t","")
+                ansDict['weather'] = line
+                continue
+            if(line.startswith("\tTags:\t")):
+                line = line.replace("\tTags:\t","")
+                ansDict['tags'] = line
+                continue
+            if(line.startswith("\tPhoto:\t")):
+                line = line.replace("\tPhoto:\t","")
+                ansDict['photo'] = line
+                continue
+            ansDict['text'] = ansDict['text'] + line + "\n"
+        return ansDict
+
+    def sentToEvernotr(self):
+        sendetNotes = set()
+        entries = sorted(self.entries,key=itemgetter('photo'),reverse= True)
+        for entry in entries:
+            self.createNoteFromEntry(entry,sendetNotes)
+
+    def createNoteFromEntry(self,entry,sendetNotes):
+
+
+        gnArgs = []
+        gnArgs.append("geeknote")
+        gnArgs.append("create")
+
+        gnArgs.append("--notebook")
+        gnArgs.append(self.notebook)
+
+        title = self.titlePrefix
+        if(entry['date'] != ""):
+            title += " created on " + entry['date'].strftime(self.dataInTitleFormat)
+        if(entry['location'] != ""):
+            title += " created in " + entry['location']
+        gnArgs.append("--title")
+        gnArgs.append(title)
+
+        if(entry['tags'] != ""):
+            gnArgs.append("--tags")
+            gnArgs.append(entry['tags'])
+        if(entry['photo'] != ""):
+            gnArgs.append("--resource")
+            gnArgs.append(self.photoDir+"/"+entry['photo'])
+        if(entry['text'] != ""):
+            gnArgs.append("--content")
+            gnArgs.append(entry['text'])
+
+        print gnArgs
+        if(entry['date'].strftime(self.dataInTitleFormat) + entry['text'] in sendetNotes):
+            print "duplicate"
+            return
+        sendetNotes.add(entry['date'].strftime(self.dataInTitleFormat) + entry['text'])
+        call( gnArgs)
 
 
 
